04d291128526f535a859c472ea39a0c09ae3b4c9
Eric Lehmann <lehm_eric@yahoo.fr>
2021-06-19 21:42:08 +0200

    set-up json server + router + views

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,61 +1,64 @@
 <template>
-    <form @submit="onSubmit" class="add-form">
-        <div class="form-control">
-            <label>Task</label>
-            <input type="text" v-model="text" name="text" placeholder="Add Task" />
-        </div>
-        <div class="form-control">
-            <label>Day & Time</label>
-            <input type="text" v-model="day" name="day" placeholder="Add Day & Time" />
-        </div>
-        <div class="form-control form-control-check">
-            <label>Set Reminder</label>
-            <input type="checkbox" v-model="reminder" name="reminder" />
-        </div>
+  <form @submit="onSubmit" class="add-form">
+    <div class="form-control">
+      <label>Task</label>
+      <input type="text" v-model="text" name="text" placeholder="Add Task" />
+    </div>
+    <div class="form-control">
+      <label>Day & Time</label>
+      <input
+        type="text"
+        v-model="day"
+        name="day"
+        placeholder="Add Day & Time"
+      />
+    </div>
+    <div class="form-control form-control-check">
+      <label>Set Reminder</label>
+      <input type="checkbox" v-model="reminder" name="reminder" />
+    </div>
 
-        <input type="submit" value="Save Task" class="btn btn-block" />
-    </form>
+    <input type="submit" value="Save Task" class="btn btn-block" />
+  </form>
 </template>
 
 <script>
-    export default {
-        name: 'AddTask',
-        data () {
-            return {
-                text: '',
-                day: '',
-                reminder: false,
-            }
-        },
-        methods: {
-            onSubmit(e) {
-                e.preventDefault()
+export default {
+  name: "AddTask",
+  data() {
+    return {
+      text: "",
+      day: "",
+      reminder: false,
+    };
+  },
+  methods: {
+    onSubmit(e) {
+      e.preventDefault();
 
-                if(!this.text) {
-                    alert('Please add a task')
-                    return
-                }
+      if (!this.text) {
+        alert("Please add a task");
+        return;
+      }
 
-                const newTask = {
-                    id: Math.floor(Math.random() * 100000),
-                    text: this.text,
-                    day: this.day,
-                    reminder: this.reminder,
-                }
+      const newTask = {
+        /* id: Math.floor(Math.random() * 100000), */
+        text: this.text,
+        day: this.day,
+        reminder: this.reminder,
+      };
 
-                this.$emit('add-task', newTask)
+      this.$emit("add-task", newTask);
 
-                this.text = ''
-                this.day = ''
-                this.reminder = false
-            }
-        }
-    }
-
+      this.text = "";
+      this.day = "";
+      this.reminder = false;
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 .add-form {
   margin-bottom: 40px;
 }
@@ -84,5 +87,4 @@
   flex: 2;
   height: 20px;
 }
-
 </style>
